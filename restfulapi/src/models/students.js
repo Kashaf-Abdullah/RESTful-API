260d91dbebfc27ffec1c91422c942f3e78670c20
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-08-03 23:05:06 +0500

    Add files via upload

--- restfulapi/src/models/students.js
+++ restfulapi/src/models/students.js
@@ -1,1 +1,41 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: "String",
+        required: "true",
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email id is already present"],
+        validate(val) {
+            if (!validator.isEmail(val)) {
+                throw new Error("invalid email")
+            }
+        }
+
+    },
+    phone:{
+        type:Number,
+        min:10,
+    
+        unique :true
+    },
+    address:{
+        type:String,
+        required:true
+    }
+
+
+})
+
+
+//create collection
+
+const Student=new mongoose.model('Student',studentSchema);
+    module.exports=Student;
 

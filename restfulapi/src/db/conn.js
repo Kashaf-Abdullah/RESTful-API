4504e8737379edab90320cf1a52af3ccaa3260fc
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-08-03 23:04:09 +0500

    Add files via upload

--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,1 +1,11 @@
+const mongoose=require("mongoose")
 
+mongoose.connect("mongodb://localhost:27017/students-api",{
+   // useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection iss succcessfully")
+}).catch((err)=>{
+    console.log(err.message)
+})
